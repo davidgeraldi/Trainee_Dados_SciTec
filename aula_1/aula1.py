3461b20c21740f2b2ee89654d7f12ade77bcca20
David Geraldi <davidregeraldi@gmail.com>
2026-05-25 18:50:52 -0300

    exercico 1 corigido para função

--- aula_1/aula1.py
+++ aula_1/aula1.py
@@ -1,11 +1,12 @@
-valores_brutos: int = [10, 50, 150, 20, 300]
-alertas: int = []
-for v in valores_brutos:
-    print(v)
-    if v > 100: 
-        alertas.append(v)
+def percorrer(lista):
+    alertas = []
+    for v in range(len(lista)):
+        if lista[v] > 100:
+            alertas.append(lista[v])
+    if len(alertas) == 0:
+        print("Sistema seguro")
+    else:
+        print(f"Revisar valores: {alertas}")
 
-if len(alertas) == 0:
-    print("Sistema seguro")
-else:
-    print(f"Revisar valores: {alertas}")
+valores_brutos = [10, 50, 150, 20, 300]
+percorrer(valores_brutos)
